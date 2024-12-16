f349a03a2eb9e12d069ad7befe8114ed11144cf3
baranov_d <baranov_d@stilsoft.ru>
2024-12-16 09:54:43 +0300

    finalized, everything works

--- main.cpp
+++ main.cpp
@@ -1,92 +1,120 @@
 #define _USE_MATH_DEFINES
-#include "svg.h" // Объявления классов библиотеки должны быть расположены в файле svg.h
-
 #include <cmath>
 #include <sstream>
+#include <vector>
+
+#include "svg.h"  // Объявления классов библиотеки должны быть расположены в файле svg.h
 
 using namespace std::literals;
 using namespace svg;
 
-namespace shapes
-{
-    class Star : public svg::Drawable
-    {
-    public:
-        Star(svg::Point center, double outer_radius, double inner_radius, int num_rays) : center_(center), outer_radius_(outer_radius), inner_radius_(inner_radius), num_rays_(num_rays) {};
+namespace shapes {
+class Star : public svg::Drawable {
+ public:
+  Star(svg::Point center, double outer_radius, double inner_radius,
+       int num_rays)
+      : center_(center),
+        outer_radius_(outer_radius),
+        inner_radius_(inner_radius),
+        num_rays_(num_rays) {};
 
-        void Draw(ObjectContainer &container) const override
-        {
-            using namespace svg;
-            Polyline polyline;
-            for (int i = 0; i <= num_rays_; ++i)
-            {
-                double angle = 2 * M_PI * (i % num_rays_) / num_rays_;
-                polyline.AddPoint({center_.x + outer_radius_ * sin(angle), center_.y - outer_radius_ * cos(angle)});
-                if (i == num_rays_)
-                {
-                    break;
-                }
-                angle += M_PI / num_rays_;
-                polyline.AddPoint({center_.x + inner_radius_ * sin(angle), center_.y - inner_radius_ * cos(angle)});
-            }
-            container.Add(polyline);
-        };
+  void Draw(ObjectContainer& container) const override {
+    using namespace svg;
+    Polyline polyline;
+    for (int i = 0; i <= num_rays_; ++i) {
+      double angle = 2 * M_PI * (i % num_rays_) / num_rays_;
+      polyline.AddPoint({center_.x + outer_radius_ * sin(angle),
+                         center_.y - outer_radius_ * cos(angle)});
+      if (i == num_rays_) {
+        break;
+      }
+      angle += M_PI / num_rays_;
+      polyline.AddPoint({center_.x + inner_radius_ * sin(angle),
+                         center_.y - inner_radius_ * cos(angle)});
+    }
+    container.Add(polyline);
+  };
 
-    private:
-        svg::Point center_;
-        double outer_radius_;
-        double inner_radius_;
-        int num_rays_;
-    };
-
-    class Triangle : public svg::Drawable
-    {
-    public:
-        Triangle(svg::Point p1, svg::Point p2, svg::Point p3)
-            : p1_(p1), p2_(p2), p3_(p3)
-        {
-        }
-
-        // Реализует метод Draw интерфейса svg::Drawable
-        void Draw(svg::ObjectContainer &container) const override
-        {
-            container.Add(svg::Polyline().AddPoint(p1_).AddPoint(p2_).AddPoint(p3_).AddPoint(p1_));
-        }
-
-    private:
-        svg::Point p1_, p2_, p3_;
-    };
-
+ private:
+  svg::Point center_;
+  double outer_radius_;
+  double inner_radius_;
+  int num_rays_;
 };
 
-Polyline CreateStar(Point center, double outer_rad, double inner_rad, int num_rays)
-{
-    Polyline polyline;
-    for (int i = 0; i <= num_rays; ++i)
-    {
-        double angle = 2 * M_PI * (i % num_rays) / num_rays;
-        polyline.AddPoint({center.x + outer_rad * sin(angle), center.y - outer_rad * cos(angle)});
-        if (i == num_rays)
-        {
-            break;
-        }
-        angle += M_PI / num_rays;
-        polyline.AddPoint({center.x + inner_rad * sin(angle), center.y - inner_rad * cos(angle)});
-    }
-    return polyline;
+class Triangle : public svg::Drawable {
+ public:
+  Triangle(svg::Point p1, svg::Point p2, svg::Point p3)
+      : p1_(p1), p2_(p2), p3_(p3) {}
+
+  // Реализует метод Draw интерфейса svg::Drawable
+  void Draw(svg::ObjectContainer& container) const override {
+    container.Add(
+        svg::Polyline().AddPoint(p1_).AddPoint(p2_).AddPoint(p3_).AddPoint(
+            p1_));
+  }
+
+ private:
+  svg::Point p1_, p2_, p3_;
+};
+
+class Snowman : public Drawable {
+ public:
+  Snowman(Point head_center, double head_radius)
+      : head_center_(head_center), head_radius_(head_radius) {}
+
+  void Draw(svg::ObjectContainer& container) const override {
+    container.Add(
+        Circle()
+            .SetCenter({head_center_.x, head_center_.y + head_radius_ * 5})
+            .SetRadius(head_radius_ * 2.0));
+    container.Add(
+        Circle()
+            .SetCenter({head_center_.x, head_center_.y + head_radius_ * 2})
+            .SetRadius(head_radius_ * 1.5));
+    container.Add(Circle().SetCenter(head_center_).SetRadius(head_radius_));
+  }
+
+ private:
+  Point head_center_;
+  double head_radius_;
+};
+};  // namespace shapes
+
+template <typename DrawableIterator>
+void DrawPicture(DrawableIterator begin, DrawableIterator end,
+                 svg::ObjectContainer& target) {
+  for (auto it = begin; it != end; ++it) {
+    (*it)->Draw(target);
+  }
 }
 
-int main()
-{
-    Document doc;
-    doc.Add(Circle().SetCenter({20, 20}).SetRadius(10));
-    doc.Add(Text()
-                .SetFontFamily("Verdana"s)
-                .SetPosition({35, 20})
-                .SetOffset({0, 6})
-                .SetFontSize(12)
-                .SetFontWeight("bold"s)
-                .SetData("Hello C++"s));
-    doc.Add(CreateStar({20, 50}, 10, 5, 5));
-    doc.Render(std::cout);
+template <typename Container>
+void DrawPicture(const Container& container, svg::ObjectContainer& target) {
+  using namespace std;
+  DrawPicture(begin(container), end(container), target);
+}
+
+int main() {
+  using namespace svg;
+  using namespace shapes;
+  using namespace std;
+
+  vector<unique_ptr<svg::Drawable>> picture;
+
+  picture.emplace_back(
+      make_unique<Triangle>(Point{100, 20}, Point{120, 50}, Point{80, 40}));
+  // 5-лучевая звезда с центром {50, 20}, длиной лучей 10 и внутренним радиусом
+  // 4
+  picture.emplace_back(make_unique<Star>(Point{50.0, 20.0}, 10.0, 4.0, 5));
+  // Снеговик с "головой" радиусом 10, имеющей центр в точке {30, 20}
+  picture.emplace_back(make_unique<Snowman>(Point{30, 20}, 10.0));
+
+  svg::Document doc;
+  // Так как документ реализует интерфейс ObjectContainer,
+  // его можно передать в DrawPicture в качестве цели для рисования
+  DrawPicture(picture, doc);
+
+  // Выводим полученный документ в stdout
+  doc.Render(cout);
 }
--- svg.h
+++ svg.h
@@ -1,179 +1,163 @@
 #pragma once
 
 #include <cstdint>
+#include <deque>
 #include <iostream>
 #include <memory>
 #include <string>
-#include <deque>
+
 #include "algorithm"
 
-namespace svg
-{
-    using namespace std::literals;
-    struct Point
-    {
-        Point() = default;
-        Point(double x, double y)
-            : x(x), y(y)
-        {
-        }
-        double x = 0;
-        double y = 0;
-    };
+namespace svg {
+using namespace std::literals;
+struct Point {
+  Point() = default;
+  Point(double x, double y) : x(x), y(y) {}
+  double x = 0;
+  double y = 0;
+};
 
-    /*
-     * Вспомогательная структура, хранящая контекст для вывода SVG-документа с отступами.
-     * Хранит ссылку на поток вывода, текущее значение и шаг отступа при выводе элемента
-     */
-    struct RenderContext
-    {
-        RenderContext(std::ostream &out)
-            : out(out)
-        {
-        }
+/*
+ * Вспомогательная структура, хранящая контекст для вывода SVG-документа с
+ * отступами. Хранит ссылку на поток вывода, текущее значение и шаг отступа при
+ * выводе элемента
+ */
+struct RenderContext {
+  RenderContext(std::ostream &out) : out(out) {}
 
-        RenderContext(std::ostream &out, int indent_step, int indent = 0)
-            : out(out), indent_step(indent_step), indent(indent)
-        {
-        }
+  RenderContext(std::ostream &out, int indent_step, int indent = 0)
+      : out(out), indent_step(indent_step), indent(indent) {}
 
-        RenderContext Indented() const
-        {
-            return {out, indent_step, indent + indent_step};
-        }
+  RenderContext Indented() const {
+    return {out, indent_step, indent + indent_step};
+  }
 
-        void RenderIndent() const
-        {
-            for (int i = 0; i < indent; ++i)
-            {
-                out.put(' ');
-            }
-        }
+  void RenderIndent() const {
+    for (int i = 0; i < indent; ++i) {
+      out.put(' ');
+    }
+  }
 
-        std::ostream &out;
-        int indent_step = 0;
-        int indent = 0;
-    };
+  std::ostream &out;
+  int indent_step = 0;
+  int indent = 0;
+};
 
-    /*
-     * Абстрактный базовый класс Object служит для унифицированного хранения
-     * конкретных тегов SVG-документа
-     * Реализует паттерн "Шаблонный метод" для вывода содержимого тега
-     */
-    class Object
-    {
-    public:
-        void Render(const RenderContext &context) const;
+/*
+ * Абстрактный базовый класс Object служит для унифицированного хранения
+ * конкретных тегов SVG-документа
+ * Реализует паттерн "Шаблонный метод" для вывода содержимого тега
+ */
+class Object {
+ public:
+  void Render(const RenderContext &context) const;
 
-        virtual ~Object() = default;
+  virtual ~Object() = default;
 
-    protected:
-        virtual void RenderObject(const RenderContext &context) const = 0;
-    };
+ protected:
+  virtual void RenderObject(const RenderContext &context) const = 0;
+};
 
-    /*
-     * Класс Circle моделирует элемент <circle> для отображения круга
-     * https://developer.mozilla.org/en-US/docs/Web/SVG/Element/circle
-     */
-    class Circle final : public Object
-    {
-    public:
-        Circle &SetCenter(Point center);
-        Circle &SetRadius(double radius);
+/*
+ * Класс Circle моделирует элемент <circle> для отображения круга
+ * https://developer.mozilla.org/en-US/docs/Web/SVG/Element/circle
+ */
+class Circle final : public Object {
+ public:
+  Circle &SetCenter(Point center);
+  Circle &SetRadius(double radius);
 
-    private:
-        void RenderObject(const RenderContext &context) const override;
+ private:
+  void RenderObject(const RenderContext &context) const override;
 
-        Point center_ = {0.0, 0.0};
-        double radius_ = 1.0;
-    };
+  Point center_ = {0.0, 0.0};
+  double radius_ = 1.0;
+};
 
-    /*
-     * Класс Polyline моделирует элемент <polyline> для отображения ломаных линий
-     * https://developer.mozilla.org/en-US/docs/Web/SVG/Element/polyline
-     */
-    class Polyline final : public Object
-    {
-    public:
-        // Добавляет очередную вершину к ломаной линии
-        Polyline &AddPoint(Point point);
+/*
+ * Класс Polyline моделирует элемент <polyline> для отображения ломаных линий
+ * https://developer.mozilla.org/en-US/docs/Web/SVG/Element/polyline
+ */
+class Polyline final : public Object {
+ public:
+  // Добавляет очередную вершину к ломаной линии
+  Polyline &AddPoint(Point point);
 
-    private:
-        void RenderObject(const RenderContext &context) const override;
-        std::deque<Point> points_;
-    };
+ private:
+  void RenderObject(const RenderContext &context) const override;
+  std::deque<Point> points_;
+};
 
-    /*
-     * Класс Text моделирует элемент <text> для отображения текста
-     * https://developer.mozilla.org/en-US/docs/Web/SVG/Element/text
-     */
-    class Text final : public Object
-    {
-    public:
-        // Задаёт координаты опорной точки (атрибуты x и y)
-        Text &SetPosition(Point pos);
+/*
+ * Класс Text моделирует элемент <text> для отображения текста
+ * https://developer.mozilla.org/en-US/docs/Web/SVG/Element/text
+ */
+class Text final : public Object {
+ public:
+  // Задаёт координаты опорной точки (атрибуты x и y)
+  Text &SetPosition(Point pos);
 
-        // Задаёт смещение относительно опорной точки (атрибуты dx, dy)
-        Text &SetOffset(Point offset);
+  // Задаёт смещение относительно опорной точки (атрибуты dx, dy)
+  Text &SetOffset(Point offset);
 
-        // Задаёт размеры шрифта (атрибут font-size)
-        Text &SetFontSize(uint32_t size);
+  // Задаёт размеры шрифта (атрибут font-size)
+  Text &SetFontSize(uint32_t size);
 
-        // Задаёт название шрифта (атрибут font-family)
-        Text &SetFontFamily(std::string font_family);
+  // Задаёт название шрифта (атрибут font-family)
+  Text &SetFontFamily(std::string font_family);
 
-        // Задаёт толщину шрифта (атрибут font-weight)
-        Text &SetFontWeight(std::string font_weight);
+  // Задаёт толщину шрифта (атрибут font-weight)
+  Text &SetFontWeight(std::string font_weight);
 
-        // Задаёт текстовое содержимое объекта (отображается внутри тега text)
-        Text &SetData(std::string data);
+  // Задаёт текстовое содержимое объекта (отображается внутри тега text)
+  Text &SetData(std::string data);
 
-    private:
-        void RenderObject(const RenderContext &context) const override;
+ private:
+  void RenderObject(const RenderContext &context) const override;
 
-        void ScreenCharacter(std::string &str, char c, std::string_view value);
+  void ScreenCharacter(std::string &str, char c, std::string_view value);
 
-        void ScreenData(std::string &str);
+  void ScreenData(std::string &str);
 
-        Point position_ = {0.0, 0.0};
-        Point offset_ = {0.0, 0.0};
-        uint32_t font_size_ = 1u;
-        std::string font_family_ = ""s;
-        std::string font_weight_ = ""s;
-        std::string data_ = ""s;
-    };
+  Point position_ = {0.0, 0.0};
+  Point offset_ = {0.0, 0.0};
+  uint32_t font_size_ = 1u;
+  std::string font_family_ = ""s;
+  std::string font_weight_ = ""s;
+  std::string data_ = ""s;
+};
 
-    class ObjectContainer
-    {
-    public:
-        // Добавляет в svg-документ объект-наследник svg::Object
+class ObjectContainer {
+ public:
+  // Добавляет в svg-документ объект-наследник svg::Object
 
-        template <typename T>
-        void Add(T obj)
-        {
-            AddPtr(std::make_unique<T>(std::move(obj)));
-        }
+  template <typename T>
+  void Add(T obj) {
+    AddPtr(std::make_unique<T>(std::move(obj)));
+  }
 
-        virtual void AddPtr(std::unique_ptr<Object> &&obj) = 0;
-    };
+  virtual void AddPtr(std::unique_ptr<Object> &&obj) = 0;
 
-    class Document : public ObjectContainer
-    {
-    public:
-        // Добавляет в svg-документ объект-наследник svg::Object
-        void AddPtr(std::unique_ptr<Object> &&obj) override;
+  virtual ~ObjectContainer() = default;
+};
 
-        // Выводит в ostream svg-представление документа
-        void Render(std::ostream &out) const;
+class Document : public ObjectContainer {
+ public:
+  // Добавляет в svg-документ объект-наследник svg::Object
+  void AddPtr(std::unique_ptr<Object> &&obj) override;
 
-    private:
-        std::deque<std::unique_ptr<Object>> objects_;
-    };
+  // Выводит в ostream svg-представление документа
+  void Render(std::ostream &out) const;
 
-    class Drawable
-    {
-    public:
-        virtual void Draw(ObjectContainer &container) const = 0;
-    };
+ private:
+  std::deque<std::unique_ptr<Object>> objects_;
+};
 
-} // namespace svg
+class Drawable {
+ public:
+  virtual void Draw(ObjectContainer &container) const = 0;
+
+  virtual ~Drawable() = default;
+};
+
+}  // namespace svg
